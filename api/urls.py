789ec47ceab8180150636550d9acb4f5275e89f5
unknown <Aman7548>
2020-12-28 20:53:33 +0530

    first commit

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path,include
+from .views import  ArticleAPIView,ArticleDetails,GenericAPIView
+
+urlpatterns = [
+    # path('article/', article_list),
+    path('article/', ArticleAPIView.as_view()),
+    path('detail/<int:id>/', ArticleDetails.as_view()),
+    path('generic/article/<int:id>/', GenericAPIView.as_view()),
+  
+]
